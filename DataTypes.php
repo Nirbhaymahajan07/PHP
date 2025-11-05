472cf6ea6b6470b6a667f142b5e6ec1932d58fb0
Nirbhaymahajan07 <nirbhaymahajan12345@gmail.com>
2025-11-05 21:24:20 +0530

    5-11-2025

---
+++ DataTypes.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>DataTypes In PHP</title>
+    <!-- Data-Types:-
+        int|float|string|boolean|array|object|null -->
+</head>
+    <body>
+    <?php
+    class Test
+    {
+
+    }
+    $a=100; //int
+    echo $a."<br>";
+    $b=155.55; //float
+    echo $b."<br>";
+    $c="Hello Mam"; //string
+    echo $c."<br>";
+    $d=TRUE; //boolean
+    echo $d."<br>";
+    $arr=array("Indore","Pune","Mumbai"); //array
+    print_r($arr);
+    $e=null; //null 
+    echo $e."<br>";
+    // t=new Test();  
+    ?> 
+</body>
+</html>
