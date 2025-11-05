77eb2fb0ef34ece485d7f7dbdfeddd429a6f3e81
Nirbhaymahajan07 <nirbhaymahajan12345@gmail.com>
2025-11-05 21:48:17 +0530

    5-11-2025

---
+++ Constant.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Constatnt PHP</title>
+</head>
+<body>
+    <?php
+    define('PI',3.14);
+    echo PI;
+    // define('PI',30.14);
+    ?>
+</body>
+</html>
