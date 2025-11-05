1653231dbae073b345dc04341ff2c0486e68ce7b
Nirbhaymahajan07 <nirbhaymahajan12345@gmail.com>
2025-11-05 20:50:43 +0530

    5-11-2025

---
+++ Add.php
@@ -0,0 +1,6 @@
+<?php
+$a=70;
+$b=60;
+$c=$a+$b;
+echo"Addition Of ".$a." & ".$b." is : ".$c;
+?>
---
+++ Index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>PHP</title>
+</head>
+<body>
+    <center>
+<marquee behavior="" direction="">This Is My First PHP Code</marquee>
+<?php
+echo"<font color=red size=5>HIIE I LOVE CRICKET</font>";
+print("<br>");
+$a=777;
+echo"Value is: ".$a;
+// print("<br>CRICKET");
+?>
+    </center>
+</body>
+</html>
