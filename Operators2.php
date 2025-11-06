b9e85a267a8e49b2b2a670d1b366343fdad70c05
Nirbhaymahajan07 <nirbhaymahajan12345@gmail.com>
2025-11-06 22:49:04 +0530

    6-11-2025

--- Operators2.php
+++ Operators2.php
@@ -4,6 +4,29 @@
     <title>PHP Operators</title>
 </head>
 <body>
-    
+    <?php
+    /*
+    &&,||,! logical Operator
+    */
+    $a=50;
+    $b=20;
+     if($a>55 && $b>10)//Both True Needed
+        echo"Hello"."<br>";
+    else
+        echo"Bye"."<br>";
+    $a=50;
+    $b=20;
+     if($a>55 || $b>10) //1 True Is  Also Ok 
+        echo"Hello"."<br>";
+    else
+        echo"Bye"."<br>";
+    $a=50;
+    $b=20;
+     if(!($a>55 || $b>10)) //True Value False Zali
+        echo"Hello"."<br>";
+    else
+        echo"Bye"."<br>";
+        
+    ?>
 </body>
 </html>
