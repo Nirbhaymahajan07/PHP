bbe48056ef48cd5962e77f412fb42c7a65923d78
Nirbhaymahajan07 <nirbhaymahajan12345@gmail.com>
2025-11-06 23:04:24 +0530

    6-11-2025

--- Operators2.php
+++ Operators2.php
@@ -25,8 +25,25 @@
      if(!($a>55 || $b>10)) //True Value False Zali
         echo"Hello"."<br>";
     else
-        echo"Bye"."<br>";
-        
+        echo"Bye"."<br>"."<br>"."<br>";
+    // ----------------------------------------------------------
+    /*
+    .,.= String Operator
+    */
+    $a=500;
+    echo"Value is:-"."$a"."<br>";
+    $a=100;
+    $str="Value is: ";
+    $str.=$a;
+    echo $str."<br>"."<br>"."<br>";
+    // ----------------------------------------------------------
+    /*
+    ?: Conditional Assignment Operator
+    */
+    $a=500;
+    $b=2000;
+    $str=$a>$b?"First Is Greater!":"Second Is Greater!!";
+    echo $str;
     ?>
 </body>
 </html>
